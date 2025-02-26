cfa6fcaa63ca3838602d4d9ecfef4d223b7254df
Stefano Sibilia <stefano.sibilia@sparkfabrik.com>
2025-02-26 17:12:14 +0100

    Add Kyverno policies and Helm chart configuration for image registry management

--- variables.tf
+++ variables.tf
@@ -1,4 +1,53 @@
-variable "name" {
+variable "namespace" {
   type        = string
-  description = "Describe what this variable is used for."
+  description = "The namespace where to install Kyverno"
+  default     = "kyverno"
+}
+
+variable "chart_version" {
+  type = string
+}
+
+variable "excluded_namespaces" {
+  type        = list(string)
+  description = "The list of namespaces to exclude from the Kyverno policies"
+  default     = []
+
+}
+
+variable "is_eks" {
+  description = "Whether the cluster is EKS or not"
+  type        = bool
+  default     = false
+}
+
+
+variable "pull_through_caches" {
+  type = map(object({
+    registry_title     = string
+    pull_through_cache = string
+  }))
+  description = "The ECR pull-through configuration for the mutating Kyverno policy. Use the registry URL as the key (e.g. 'index.docker.io'), registry_title as the name used to create the title in the policy, and pull_through_cache as the ECR pull-through cache URL."
+
+  validation {
+    condition = alltrue([
+      for key, value in var.pull_through_caches : can(regex("^[a-zA-Z0-9-]+$", value.registry_title))
+    ])
+    error_message = "The registry_title must be alphanumeric, can contain hyphens only (use to create Kubernetes resource names, RFC 1123)"
+  }
+}
+
+variable "artifact_registry_remote_mirrors" {
+  type = map(object({
+    registry_title                  = string
+    artifact_registry_remote_mirror = string
+  }))
+  description = "The ECR pull-through configuration for the mutating Kyverno policy. Use the registry URL as the key (e.g. 'index.docker.io'), registry_title as the name used to create the title in the policy, and pull_through_cache as the ECR pull-through cache URL."
+
+  validation {
+    condition = alltrue([
+      for key, value in var.artifact_registry_remote_mirrors : can(regex("^[a-zA-Z0-9-]+$", value.registry_title))
+    ])
+    error_message = "The registry_title must be alphanumeric, can contain hyphens only (use to create Kubernetes resource names, RFC 1123)"
+  }
 }
--- versions.tf
+++ versions.tf
@@ -6,5 +6,17 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 5.13"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.3"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.0"
+    }
   }
 }
