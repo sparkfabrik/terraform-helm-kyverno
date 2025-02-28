b28b556a531e1246bf0e0b3fa948ee03bacb7e06
Stefano Sibilia <stefano.sibilia@sparkfabrik.com>
2025-02-28 10:20:12 +0100

    platform/3450: Add output variables for admission, background, cleanup, and reports controllers

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,21 @@
+output "admissioncontroller_sa" {
+  value = var.admissioncontroller_sa
+}
+
+output "backgroundcontroller_sa" {
+  value = var.backgroundcontroller_sa
+}
+
+output "cleanupcontroller_sa" {
+  value = var.cleanupcontroller_sa
+}
+
+output "reportscontroller_sa" {
+  value = var.reportscontroller_sa
+}
+
+output "dockerhub_mirror_registry" {
+  value = var.policy_docker_hub_mirror.destination_registry
+
+}
 
